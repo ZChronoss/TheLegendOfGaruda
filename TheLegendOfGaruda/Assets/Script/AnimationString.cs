f7ac032e32d859c0f9ece98ffa14b1ae6f6585b4
ZChronoss <renaldi.antonio19@gmail.com>
2024-12-29 00:47:06 +0700

    feat: jump and falling animation

--- TheLegendOfGaruda/Assets/Script/AnimationString.cs
+++ TheLegendOfGaruda/Assets/Script/AnimationString.cs
@@ -4,4 +4,7 @@ public class AnimationString
 {
     internal static string attackTrigger = "attack";
     internal static string canMove = "canMove";
+    internal static string isGrounded = "isGrounded";
+    internal static string yVelocity = "yVelocity";
+    internal static string jump = "jump";
 }
