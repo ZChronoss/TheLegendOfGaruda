4e6d6fd3a2c311708e2d85f755d4fdfc4202eae2
LeoLodra <leonardomarhan5@gmail.com>
2025-01-07 20:01:12 +0700

    feat: added flying dragon

--- TheLegendOfGaruda/Assets/Script/EnemyShooting.cs
+++ TheLegendOfGaruda/Assets/Script/EnemyShooting.cs
@@ -7,27 +7,52 @@ public class EnemyShooting : MonoBehaviour
     public GameObject bullet;
     public Transform bulletPos;
     private GroundedEnemyMovement controller;
+    private FlyingEnemyMovement flyingController;
+    private bool flyingEnemy = false;
     private float timer;
     public bool shootingDisabled = false;
     public float shootingCooldown = 2f;
     
-    void Start(){
+    void Start()
+    {
         controller = GetComponent<GroundedEnemyMovement>();
+        if (!controller) 
+        {
+            flyingController = GetComponent<FlyingEnemyMovement>();
+            flyingEnemy = true;
+        }
     }
-    // Update is called once per frame
+
     void Update()
     {
-        if (controller.isChasing && !shootingDisabled){
+        if (CanShoot()) 
+        {
             timer += Time.deltaTime;
 
-            if(timer>shootingCooldown){
+            if (timer > shootingCooldown) 
+            {
                 timer = 0;
                 shoot();
             }
         }
     }
 
-    void shoot(){
-        Instantiate(bullet, bulletPos.position, Quaternion.identity);
+    private bool CanShoot()
+    {
+        if (flyingEnemy && flyingController != null)
+            return flyingController.isChasing && !shootingDisabled;
+
+        if (!flyingEnemy && controller != null)
+            return controller.isChasing && !shootingDisabled;
+
+        return false;
+    }
+
+    void shoot()
+    {
+        if (bullet && bulletPos)
+        {
+            Instantiate(bullet, bulletPos.position, Quaternion.identity);
+        }
     }
 }
