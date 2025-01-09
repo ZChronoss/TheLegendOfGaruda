d91606bdb21bdde9890fefb1dd769880f218303c
LeoLodra <leonardomarhan5@gmail.com>
2025-01-09 14:43:04 +0700

    fix: playerHealth sometimes null fixed

--- TheLegendOfGaruda/Assets/Script/EnemyDamage.cs
+++ TheLegendOfGaruda/Assets/Script/EnemyDamage.cs
@@ -1,37 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MonsterDamage : MonoBehaviour
 {
     public int damage = 1;
     private PlayerHealth playerHealth;
-    public void Start(){
+
+    private void Start()
+    {
+        FindPlayer();
+    }
+
+    private void FindPlayer()
+    {
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         if (player != null)
         {
-           playerHealth = player.GetComponent<PlayerHealth>();
-           if (playerHealth == null)
-            {
-                Debug.LogError("PlayerHealth component is missing on the Player GameObject!");
-            }
-        }
-        else
-        {
-            Debug.LogError("No GameObject with the 'Player' tag found in the scene!");
+            playerHealth = player.GetComponent<PlayerHealth>();
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) {
-        if(collision.gameObject.tag == "Player"){
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (playerHealth == null)
+            {
+                Debug.LogWarning("PlayerHealth is null. Attempting to find the player again.");
+                FindPlayer();
+            }
+
             if (playerHealth != null)
             {
                 Debug.Log("Player hit! Applying damage.");
                 playerHealth.takeDamage(damage);
-            }
-            else
-            {
-                Debug.LogError("playerHealth is null. Damage cannot be applied.");
             }
         }
     }
